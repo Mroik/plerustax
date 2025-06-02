66fa2998a5ae612506d197f432bceb680a1e9fe3
Mroik <mroik@delayed.space>
2025-06-02 03:15:02 +0200

    Add home timeline fetching

--- src/pleroma/backend.rs
+++ src/pleroma/backend.rs
@@ -15,6 +15,83 @@ struct TokenResponse {
     access_token: String,
 }
 
+#[derive(Deserialize)]
+pub struct AccountField {
+    name: String,
+    value: String,
+}
+
+#[derive(Deserialize)]
+pub struct Account {
+    id: String,
+    acct: String,
+    display_name: String,
+    bot: bool,
+    note: String,
+    url: String,
+    follower_count: u32,
+    following_count: u32,
+    statuses_count: u32,
+    fields: Vec<AccountField>,
+}
+
+// TODO
+#[derive(Deserialize)]
+pub struct MediaAttatchment {}
+
+#[derive(Deserialize)]
+pub struct TweetMention {
+    id: String,
+    acct: String,
+    url: String,
+}
+
+#[derive(Deserialize)]
+pub struct TweetTag {
+    name: String,
+}
+
+#[derive(Deserialize)]
+pub struct PollOption {
+    title: String,
+    votes_count: u32,
+}
+
+#[derive(Deserialize)]
+pub struct Poll {
+    id: String,
+    expires_at: String,
+    expired: bool,
+    multiple: bool,
+    votes_count: u32,
+    voters_count: Option<u32>,
+}
+
+#[derive(Deserialize)]
+pub struct Tweet {
+    id: String,
+    created_at: String,
+    in_response_to_id: Option<String>,
+    in_reply_to_account_id: Option<String>,
+    sensitive: bool,
+    spoiler_text: String,
+    visibility: String,
+    uri: String,
+    replies_count: u32,
+    reblog_count: u32,
+    favourites_count: u32,
+    favourited: bool,
+    reblogged: bool,
+    muted: bool,
+    content: String,
+    reblog: Option<Box<Tweet>>,
+    account: Account,
+    media_attachments: Vec<MediaAttatchment>,
+    mentions: Vec<TweetMention>,
+    tags: Vec<TweetTag>,
+    poll: Option<Poll>,
+}
+
 #[derive(Debug)]
 pub struct Backend {
     base_url: String,
@@ -92,6 +169,16 @@ impl Backend {
         self.token = Some(data.access_token.clone());
 
         Ok(())
+    }
+
+    pub async fn home_timeline(&self, from_id: Option<&str>) -> Result<Vec<Tweet>> {
+        let mut req = self.http.get("{}/api/v1/timelines/home");
+        if from_id.is_some() {
+            req = req.query(&[("since_id", from_id.unwrap())]);
+        }
+        let res = req.send().await?;
+        let data: Vec<Tweet> = res.json().await?;
+        Ok(data)
     }
 }
 
